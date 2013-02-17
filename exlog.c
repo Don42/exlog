a831bba356c88965d13989348752c2771091c350
Marco don Kaulea <DonMarco42@gmail.com>
2013-02-17 19:43:12 +0100

    Adds Setup function and command parsing

--- exlog.c
+++ exlog.c
@@ -8,8 +8,117 @@
 
 #define _GNU_SOURCE
 
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#include "exlog.h"
+
+void usage(void);
+void setup(void);
+
 int
 main(int argc, char *argv[])
 {
+    int ch, cmd;
+    const char *commands[]={"add", "rm", NULL};
+    enum {ADD,RM};
+    if (argc <= 1)
+    {
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = getopt(argc, argv, "h")) != -1)
+    {
+        switch (ch)
+        {
+            case 'h':
+                usage();
+                exit(EXIT_SUCCESS);
+            default:
+                usage();
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    setup();
+
+    for (cmd=0;commands[cmd] && strcasecmp(argv[1],commands[cmd]);cmd++);
+    switch (cmd)
+    {
+        case ADD:
+            break;
+        case RM:
+            break;
+        default:
+            fprintf(stderr, "Unknown command...");
+            exit(EXIT_FAILURE);
+            break;
+    }
     return (0);
+}
+
+void
+usage(void)
+{
+    fprintf(stderr, "usage: exlog [command] [-h]\n");
+}
+
+void
+setup(void)
+{
+    struct stat sts;
+    char *home = getenv("HOME");
+    if (home != NULL)
+    {
+        size_t sizeNeeded = snprintf (NULL, 0, DEFAULT_FOLDER, home);
+        char *path = malloc (sizeNeeded);
+        snprintf (path, sizeNeeded, DEFAULT_FOLDER, home);
+        if ((stat(path, &sts)) == -1)
+        {
+            printf ("Storage folder not found.\n"
+                    "Creating folder \"%s\" now...\n", path);
+            if (mkdir (path, 0700) != 0)
+            {
+                fprintf(stderr, "Failed to create storage folder: ");
+                switch(errno)
+                {
+                    case EACCES:
+                        fprintf(stderr, "EACCES\n");
+                        break;
+                    case EEXIST:
+                        fprintf(stderr, "EEXIST\n");
+                        break;
+                    case EFAULT:
+                        fprintf(stderr, "EFAULT\n");
+                        break;
+                    case EMLINK:
+                        fprintf(stderr, "EMLINK\n");
+                        break;
+                    case ENAMETOOLONG:
+                        fprintf(stderr, "ENAMETOOLONG\n");
+                        break;
+                    case ENOENT:
+                        fprintf(stderr, "ENOENT\n");
+                        break;
+                    case ENOSPC:
+                        fprintf(stderr, "ENOSPC\n");
+                        break;
+                    case ENOTDIR:
+                        fprintf(stderr, "ENOTDIR\n");
+                        break;
+                    case EPERM:
+                        fprintf(stderr, "EPERM\n");
+                        break;
+                    case EROFS:
+                        fprintf(stderr, "EROFS\n");
+                        break;
+                }
+            }
+        }
+    }
 }
